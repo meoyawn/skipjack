18883cddf2b82cc8398ed74aef8cb4638d3f67e4
adelnizamutdinov <stiggpwnz@gmail.com>
2014-10-19 04:43:48 +0400

    extracting h0 constant

--- DaviesMeyer.hs
+++ DaviesMeyer.hs
@@ -24,5 +24,8 @@ encryptFold :: Word16x4 -> ByteString -> Word16x4
 encryptFold h@(h1, h2, h3, h4) mi = (e1 `xor` h1, e2 `xor` h2, e3 `xor` h3, e4 `xor` h4)
     where (e1, e2, e3, e4) = encryptBlock mi h
 
+h0 :: Word16x4
+h0 = (3123, 6778, 5679, 6788)
+
 daviesMeyer :: String -> Word16x4
-daviesMeyer s = foldl encryptFold (3123, 6778, 5679, 6788) (keys s)
+daviesMeyer s = foldl encryptFold h0 (keys s)
