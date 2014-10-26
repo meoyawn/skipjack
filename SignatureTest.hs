4d6cc529751a58136fa9b506507bc58bc7947c48
adelnizamutdinov <stiggpwnz@gmail.com>
2014-10-26 21:02:19 +0300

    going back to vector

--- SignatureTest.hs
+++ SignatureTest.hs
@@ -5,9 +5,6 @@ module SignatureTest where
 
 import Signature
 import Hash
-import CBC
-import Control.Applicative
-import Data.ByteString hiding (length, reverse, take, drop, last, head, init)
 import Test.QuickCheck
 
 data Sig = Sig Integer Integer deriving (Show, Eq)
@@ -21,23 +18,6 @@ instance Arbitrary Sig where
 prop_verify :: String -> Sig -> Bool
 prop_verify str (Sig k x) = verify h y $ signature h k x
     where h = hash str
-          y = calculateY x
-
-instance Arbitrary ByteString where
-    arbitrary = pack <$> vector 10
-
-prop_total :: ByteString -> String -> Sig -> Bool
-prop_total key str (Sig k x) = verify decHash y (decR, decS)
-    where enc = encrypt key $ str ++ "\n" ++ rStr ++ "\n" ++ sStr
-          h = hash str
-          (r, s) = signature h k x
-          rStr = show r
-          sStr = show s
-          dec = decrypt key enc
-          decR = read . head . drop 1 . reverse . lines $ dec
-          decS = read . last . lines $ dec
-          decMsg = init . unlines . reverse . drop 2 . reverse . lines $ dec
-          decHash = hash decMsg
           y = calculateY x
 
 return []
